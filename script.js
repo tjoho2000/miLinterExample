ee8094fb3bf986f37478ad90118f7cf5068a6c46
tjoho2000 <tjoho2000@hotmail.com>
2024-05-20 21:59:57 +0200

    lint fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const x = 5
+const y = 5
 
-function double(x) { return x * 2 }
+function double (x) { return x * 2 }
 
 console.print('Dubbelt av 5 är ' + double(x))
+console.print('Dubbelt av 7 är ' + double(y))
